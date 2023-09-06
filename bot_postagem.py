ebc1ae2bee893f01f73677f45608b4130ae7959f
Daniel Antunes <daniel.antunes.nutri@gmail.com>
2023-09-05 22:21:45 -0300

    Mudanças em funções digitar_devgar, criação de função para ler txt, modificação na inserção do texto através de arquivo txt, etc...

--- bot_postagem.py
+++ bot_postagem.py
@@ -1,15 +1,16 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from time import sleep
-from funcoes import iniciar_driver, digitar_devagar, espera_aleatoria
+from funcoes import iniciar_driver, digitar_devagar, espera_aleatoria, ler_txt
 
-texto = str(input('Cole aqui o texto a ser publicado: ')).strip()
-print('Especifique o caminho da mídia com o nome do arquivo, ou', end=' ')
-diretorio = str(input('deixe em branco para publicar apenas texto: ')).strip()
+diretorio_texto = str(input('Especifique o diretório do texto com o nome do arquivo .txt: ')).strip()
+texto = ler_txt(nome_arquivo=diretorio_texto)
+diretorio_midia = str(input('Especifique o diretório da mídia com o nome do arquivo, '
+                      'ou deixe em branco para publicar apenas texto: ')).strip()
 
 wait, driver = iniciar_driver()
 driver.get('https://www.linkedin.com/')
-espera_aleatoria(4, 9.0)
+espera_aleatoria(2.0, 4.5)
 
 # Escrevendo o texto
 iniciar_publicacao = wait.until(expected_conditions.element_to_be_clickable((
@@ -25,13 +26,13 @@ digitar_devagar(campo_texto, texto)
 espera_aleatoria()
 
 # Adicionando mídia
-if diretorio != '':
+if diretorio_midia != '':
     botao_midia = wait.until(expected_conditions.element_to_be_clickable((
         By.XPATH, '//button[@aria-label="Adicionar mídia"]')))
     botao_midia.click()
     enviar_midia = driver.find_element(By.ID, 'media-editor-file-selector__file-input')
     espera_aleatoria()
-    enviar_midia.send_keys(diretorio)
+    enviar_midia.send_keys(diretorio_midia)
     espera_aleatoria()
     sleep(1)
     botao_avancar = wait.until(expected_conditions.element_to_be_clickable((
--- funcoes.py
+++ funcoes.py
@@ -10,7 +10,7 @@ def iniciar_driver():
     chrome_options = Options()
     # chrome_options.binary_location = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
     arguments = ['--lang=pt-BR', 'window-size=1300,1000',
-                 '--user-data-dir=C:\\Users\\Daniel\\AppData\\Local\\Google\\Chrome\\User Data',
+                 '--user-data-dir=C:\\Users\\Daniel\\AppData\\Local\\Google\\Chrome\\User Data'
                  '--headless']
     for argument in arguments:
         chrome_options.add_argument(argument)
@@ -23,7 +23,7 @@ def iniciar_driver():
     driver = webdriver.Chrome(options=chrome_options)
     wait = WebDriverWait(
         driver,
-        10,
+        20,
         poll_frequency=1,  # frequencia q vai tentar fazer algo
         ignored_exceptions=[
             NoSuchElementException,
@@ -37,9 +37,27 @@ def iniciar_driver():
 
 def digitar_devagar(campo, msg):
     for letra in msg:
-        sleep(randint(1, 5)/30)
+        espera_aleatoria(0.01, 0.5 / 100)
         campo.send_keys(letra)
 
 
-def espera_aleatoria(inicio=2.5, fim=6.5):
+def espera_aleatoria(inicio=2.5, fim=5.5):
     sleep(uniform(inicio, fim))
+
+
+def ler_txt(nome_arquivo):
+    """
+    Essa função serve para ler arquivos de texto, guardando em uma variável e retornando esse texto.
+    :param nome_arquivo: Podem ser passados o nome do arquivo ou o diretório e o nome do arquivo, separados por duplas
+    barras invertidas. Deve ser passado como uma string, e com o formato .txt no final.
+    Exemplo: 'texto_para_ler.txt' ou 'C:\\Users\\[seu_usuario]\\Documents\\texto_para_ler.txt'
+    :return: retorna o texto que estava escrito no arquivo.txt
+    """
+    with open(nome_arquivo, 'r', encoding='UTF-8') as arquivo_texto:
+        texto = arquivo_texto.read()
+        return texto
+
+
+if __name__ == '__main__':
+    txt = ler_txt(nome_arquivo='texto_a_publicar.txt')
+    print(txt)
